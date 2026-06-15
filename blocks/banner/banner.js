afa1bcc54533a0d8162d807d69f1b51da80f0d5e
siyona-sn <ssnotra@adobe.com>
2026-06-15 11:42:34 +0530

    Assignment 4- Banner Added

---
+++ blocks/banner/banner.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+const row = block.querySelector(':scope > div');
+
+if (!row) return;
+
+const imageWrapper = row.children[0];
+const contentWrapper = row.children[1];
+
+if (imageWrapper) {
+imageWrapper.classList.add('banner-image');
+}
+
+if (contentWrapper) {
+contentWrapper.classList.add('banner-content');
+}
+
+const cta = contentWrapper?.querySelector('a');
+if (cta) {
+cta.classList.add('banner-btn');
+}
+}
